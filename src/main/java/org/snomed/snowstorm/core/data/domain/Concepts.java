af1739b3123918d7da3af2163d73a365a54724e9
Kai Kewley <kaikewley@gmail.com>
2023-01-09 18:05:13 +0000

    ISTO-65 Finding/Procedure context transformations.

--- src/main/java/org/snomed/snowstorm/core/data/domain/Concepts.java
+++ src/main/java/org/snomed/snowstorm/core/data/domain/Concepts.java
@@ -8,6 +8,9 @@ import java.util.Set;
 
 public class Concepts {
 
+	private Concepts() {
+	}
+
 	public static final String SNOMEDCT_ROOT = "138875005";
 
 	// Modules
@@ -196,6 +199,7 @@ public class Concepts {
 
 	// Notable concepts
 	public static final String CLINICAL_FINDING = "404684003";
+	public static final String PROCEDURE = "71388002";
 	public static final String SUBSTANCE = "105590001";
 	public static final String FINDING_SITE = "363698007";
 	public static final String HEART_STRUCTURE = "80891009";
@@ -203,4 +207,11 @@ public class Concepts {
 	public static final String CONCEPT_TYPE_COMPONENT = "900000000000461009";
 
 	public static final String MODULE_DEPENDENCY_REFERENCE_SET = "900000000000534007";
+	public static final String FINDING_CONTEXT = "408729009";
+	public static final String PROCEDURE_CONTEXT = "408730004";
+	public static final String TEMPORAL_CONTEXT = "408731000";
+	public static final String SUBJECT_RELATIONSHIP_CONTEXT = "408732007";
+	public static final String SITUATION_WITH_EXPLICIT_CONTEXT = "243796009";
+	public static final String ASSOCIATED_FINDING = "246090004";
+	public static final String ASSOCIATED_PROCEDURE = "363589002";
 }
