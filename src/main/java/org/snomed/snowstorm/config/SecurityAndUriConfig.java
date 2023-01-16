8c52b854fee8cbc302d6bb3a9164645d8ddd8296
Kai Kewley <kaikewley@gmail.com>
2023-01-16 13:23:05 +0000

    ISTO-65 Remove native API expression controller

--- src/main/java/org/snomed/snowstorm/config/SecurityAndUriConfig.java
+++ src/main/java/org/snomed/snowstorm/config/SecurityAndUriConfig.java
@@ -121,7 +121,6 @@ public class SecurityAndUriConfig {
 				"/(.*)/relationships.*",
 				"/(.*)/descriptions.*",
 				"/(.*)/members.*",
-				"/(.*)/expressions.*",
 				"/(.*)/identifiers",
 				"/(.*)/identifiers/.*",
 				"/(.*)/classifications.*",
