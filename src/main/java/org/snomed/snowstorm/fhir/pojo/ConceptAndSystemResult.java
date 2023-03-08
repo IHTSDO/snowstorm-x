a450311b674db64b49341c9e68e5c152d62f535c
Kai Kewley <kaikewley@gmail.com>
2023-03-08 14:28:31 +0000

    ISTO-82 CS $validate-code for postcoordinated SCT
    CodeSystem $validate-code operation for SNOME CT Postcoordinated Expressions.

--- src/main/java/org/snomed/snowstorm/fhir/pojo/ConceptAndSystemResult.java
+++ src/main/java/org/snomed/snowstorm/fhir/pojo/ConceptAndSystemResult.java
@@ -3,5 +3,36 @@ package org.snomed.snowstorm.fhir.pojo;
 import org.snomed.snowstorm.core.data.domain.Concept;
 import org.snomed.snowstorm.fhir.domain.FHIRCodeSystemVersion;
 
-public record ConceptAndSystemResult(Concept concept, FHIRCodeSystemVersion codeSystemVersion) {
+public class ConceptAndSystemResult {
+
+	private final Concept concept;
+	private final FHIRCodeSystemVersion codeSystemVersion;
+	private String message;
+
+	public ConceptAndSystemResult(Concept concept, FHIRCodeSystemVersion codeSystemVersion) {
+		this.concept = concept;
+		this.codeSystemVersion = codeSystemVersion;
+	}
+
+	public ConceptAndSystemResult(Concept concept, FHIRCodeSystemVersion codeSystemVersion, String message) {
+		this.concept = concept;
+		this.codeSystemVersion = codeSystemVersion;
+		this.message = message;
+	}
+
+	public Concept getConcept() {
+		return concept;
+	}
+
+	public FHIRCodeSystemVersion getCodeSystemVersion() {
+		return codeSystemVersion;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
 }
