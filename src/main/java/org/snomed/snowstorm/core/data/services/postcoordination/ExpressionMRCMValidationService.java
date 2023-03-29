1e0101c31312773b847922048c5c1cc588aab644
Kai Kewley <kaikewley@gmail.com>
2023-03-29 14:31:46 +0100

    ISTO-81 Transform error and logging improvements

--- src/main/java/org/snomed/snowstorm/core/data/services/postcoordination/ExpressionMRCMValidationService.java
+++ src/main/java/org/snomed/snowstorm/core/data/services/postcoordination/ExpressionMRCMValidationService.java
@@ -1,12 +1,12 @@
 package org.snomed.snowstorm.core.data.services.postcoordination;
 
+import ch.qos.logback.classic.Level;
 import org.snomed.languages.scg.domain.model.Attribute;
 import org.snomed.languages.scg.domain.model.AttributeGroup;
 import org.snomed.languages.scg.domain.model.AttributeValue;
 import org.snomed.languages.scg.domain.model.Expression;
 import org.snomed.snowstorm.core.data.domain.ConceptMini;
 import org.snomed.snowstorm.core.data.services.ConceptService;
-import org.snomed.snowstorm.core.data.services.NotFoundException;
 import org.snomed.snowstorm.core.data.services.ServiceException;
 import org.snomed.snowstorm.core.data.services.postcoordination.model.ComparableExpression;
 import org.snomed.snowstorm.core.util.TimerUtil;
@@ -58,7 +58,7 @@ public class ExpressionMRCMValidationService {
 							.collect(Collectors.toSet());
 					List<String> validDomainsForUsedAttribute = conceptService.findConceptMinis(context.getBranchCriteria(), validDomains, DEFAULT_LANGUAGE_DIALECTS)
 							.getResultsMap().values().stream().map(ConceptMini::getIdAndFsnTerm).toList();
-					throw new IllegalArgumentException(format("Attribute Type %s can not be used with the given focus concepts %s because the attribute can only be used " +
+					throw new ExpressionValidationException(format("Attribute Type %s can not be used with the given focus concepts %s because the attribute can only be used " +
 							"in the following MRCM domains: %s.", usedAttribute.getAttributeId(), focusConceptIds, validDomainsForUsedAttribute));
 				}
 			}
@@ -100,7 +100,7 @@ public class ExpressionMRCMValidationService {
 			// Active attribute exists in MRCM
 			if (!attributeDomainAttributeIds.contains(attributeId)) {
 				Map<String, String> conceptIdAndFsnTerm = getConceptIdAndTerm(Collections.singleton(attributeId), context);
-				throw new IllegalArgumentException(format("Attribute %s is not found in the MRCM rules.", conceptIdAndFsnTerm.get(attributeId)));
+				throw new ExpressionValidationException(format("Attribute %s is not found in the MRCM rules.", conceptIdAndFsnTerm.get(attributeId)));
 			}
 			AttributeValue attributeValue = attribute.getAttributeValue();
 			if (!attributeValue.isNested()) {
@@ -116,7 +116,7 @@ public class ExpressionMRCMValidationService {
 
 	private void assertAttributeValueWithinRange(String attributeId, String attributeValueId, ExpressionContext context) throws ServiceException {
 		// Value within attribute range
-		TimerUtil timer = new TimerUtil("attribute validation");
+		TimerUtil timer = new TimerUtil("attribute validation", Level.INFO, 5);
 		Collection<Long> longs = mrcmService.retrieveAttributeValueIds(CONTENT_TYPE, attributeId, attributeValueId,
 				context.getBranch(), null, context.getBranchMRCM(), context.getMRCMBranchCriteria());
 		timer.checkpoint("retrieveAttributeValueIds");
@@ -138,7 +138,7 @@ public class ExpressionMRCMValidationService {
 					}
 					buffer.append("(").append(mandatoryAttributeRange.getRangeConstraint()).append(")");
 				}
-				throw new IllegalArgumentException(format("Value %s is not within the permitted range of attribute %s - %s.",
+				throw new ExpressionValidationException(format("Value %s is not within the permitted range of attribute %s - %s.",
 						attributeValueFromStore, conceptIdAndFsnTerm.get(attributeId), buffer));
 			}
 		}
@@ -150,7 +150,7 @@ public class ExpressionMRCMValidationService {
 		return resultsMap.values().stream().collect(Collectors.toMap(ConceptMini::getConceptId, ConceptMini::getIdAndFsnTerm));
 	}
 
-	private void throwConceptNotFound(String concept) {
-		throw new NotFoundException(format("Concept %s not found on this branch.", concept));
+	private void throwConceptNotFound(String concept) throws ExpressionValidationException {
+		throw new ExpressionValidationException(format("Concept %s not found on this branch.", concept));
 	}
 }
