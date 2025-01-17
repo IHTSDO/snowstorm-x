37f04685ccd4401a713726d73417f259f7435328
Bart Decuypere (eHealth) <90335317+bdc-ehealth@users.noreply.github.com>
2025-01-17 15:00:32 +0100

    MAINT-2877 Add support for CodeSystem properties and SNOMED Supplements

--- src/main/java/org/snomed/snowstorm/fhir/domain/FHIRConcept.java
+++ src/main/java/org/snomed/snowstorm/fhir/domain/FHIRConcept.java
@@ -4,8 +4,7 @@ import ca.uhn.fhir.jpa.entity.TermConcept;
 import ca.uhn.fhir.jpa.entity.TermConceptDesignation;
 import ca.uhn.fhir.jpa.entity.TermConceptParentChildLink;
 import ca.uhn.fhir.jpa.entity.TermConceptProperty;
-import org.apache.thrift.Option;
-import org.hl7.fhir.r4.model.CodeSystem;
+import org.hl7.fhir.r4.model.*;
 import org.snomed.snowstorm.core.data.domain.ConceptMini;
 import org.snomed.snowstorm.core.data.domain.Description;
 import org.snomed.snowstorm.core.pojo.LanguageDialect;
@@ -25,6 +24,8 @@ import static org.snomed.snowstorm.fhir.config.FHIRConstants.SNOMED_URI;
 @Document(indexName = "#{@indexNameProvider.indexName('fhir-concept')}", createIndex = false)
 public class FHIRConcept implements FHIRGraphNode {
 
+	public static final String EXTENSION_MARKER = "://";
+
 	public interface Fields {
 
 		String CODE_SYSTEM_VERSION = "codeSystemVersion";
@@ -34,6 +35,7 @@ public class FHIRConcept implements FHIRGraphNode {
 		String PARENTS = "parents";
 		String ANCESTORS = "ancestors";
 		String PROPERTIES = "properties";
+		String EXTENSIONS = "extensions";
 	}
 	@Id
 	// Internal ID
@@ -63,6 +65,8 @@ public class FHIRConcept implements FHIRGraphNode {
 
 	private Map<String, List<FHIRProperty>> properties;
 
+	private Map<String, List<FHIRProperty>> extensions;
+
 	public FHIRConcept() {
 		active = true;
 	}
@@ -81,9 +85,15 @@ public class FHIRConcept implements FHIRGraphNode {
 		}
 
 		properties = new HashMap<>();
-		for (TermConceptProperty property : termConcept.getProperties()) {
+		for (TermConceptProperty property : termConcept.getProperties().stream().filter(p -> !p.getKey().contains(EXTENSION_MARKER)).toList()) {
 			properties.computeIfAbsent(property.getKey(), (i) -> new ArrayList<>())
 					.add(new FHIRProperty(property));
+		}
+
+		extensions = new HashMap<>();
+		for (TermConceptProperty extension : termConcept.getProperties().stream().filter(p -> p.getKey().contains(EXTENSION_MARKER)).toList()) {
+			extensions.computeIfAbsent(extension.getKey(), (i) -> new ArrayList<>())
+					.add(new FHIRProperty(extension));
 		}
 
 		parents = new HashSet<>();
@@ -112,9 +122,37 @@ public class FHIRConcept implements FHIRGraphNode {
 				.filter(FHIRConcept::isPropertyInactive)
 				.findFirst().ifPresentOrElse(x -> active = false, ()-> active = true);
 		properties.put("inactive",Collections.singletonList(new FHIRProperty("inactive",null,Boolean.toString(!isActive()),FHIRProperty.BOOLEAN)));
+		extensions = new HashMap<>();
+		definitionConcept.getExtension().forEach(
+				e ->{
+					Optional.ofNullable(extensions.get(e.getUrl())).ifPresentOrElse(list ->{
+						list.add(new FHIRProperty(e.getUrl(), null,e.getValue().primitiveValue(), FHIRProperty.typeToFHIRPropertyType(e.getValue())));
+
+					}, ()->{
+						List<FHIRProperty> list = new ArrayList<>();
+						list.add(new FHIRProperty(e.getUrl(), null,e.getValue().primitiveValue(), FHIRProperty.typeToFHIRPropertyType(e.getValue())));
+						extensions.put(e.getUrl(), list);
+					});
+
+				}
+		);
 		parents = new HashSet<>();
 		for (CodeSystem.ConceptPropertyComponent propertyComponent : definitionConcept.getProperty()) {
-			properties.computeIfAbsent(propertyComponent.getCode(), k -> new ArrayList<>()).add(new FHIRProperty(propertyComponent));
+			if (properties.get(propertyComponent.getCode())==null && !propertyComponent.getCode().contains(EXTENSION_MARKER)){
+				properties.put(propertyComponent.getCode(),new ArrayList<>());
+			}
+			try{
+				if(!propertyComponent.getCode().contains(EXTENSION_MARKER)){
+					properties.get(propertyComponent.getCode()).add(new FHIRProperty(propertyComponent));
+				}
+			} catch( UnsupportedOperationException e){
+				List<FHIRProperty> unmodifiableList = properties.get(propertyComponent.getCode());
+				List<FHIRProperty> modifiableList = new ArrayList<>();
+				modifiableList.addAll(unmodifiableList);
+				properties.put(propertyComponent.getCode(), modifiableList);
+				properties.get(propertyComponent.getCode()).add(new FHIRProperty(propertyComponent));
+			}
+
 			if (propertyComponent.getCode().equals("parent") || propertyComponent.getCode().equals("subsumedBy")) {
 				parents.add(propertyComponent.hasValueCoding() ? propertyComponent.getValueCoding().getCode() : propertyComponent.getValue().toString());
 			}
--- src/main/java/org/snomed/snowstorm/fhir/services/FHIRValueSetProviderHelper.java
+++ src/main/java/org/snomed/snowstorm/fhir/services/FHIRValueSetProviderHelper.java
@@ -59,8 +59,11 @@ class FHIRValueSetProviderHelper {
 					findParameterCanonicalOrNull(parametersParameterComponents, "system-version"),
 					findParameterCanonicalOrNull(parametersParameterComponents, "check-system-version"),
 					findParameterCanonicalOrNull(parametersParameterComponents, "force-system-version"),
-					findParameterStringOrNull(parametersParameterComponents, "version"));
-
+					findParameterStringOrNull(parametersParameterComponents, "version"),
+					findParameterStringOrNull(parametersParameterComponents, "property"));
+		} catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	static ValueSetExpansionParameters getValueSetExpansionParameters(
@@ -85,7 +88,8 @@ class FHIRValueSetProviderHelper {
 			final StringType systemVersion,
 			final StringType checkSystemVersion,
 			final StringType forceSystemVersion,
-			final StringType version) {
+			final StringType version,
+			final CodeType property) {
 
 		try {
 			return new ValueSetExpansionParameters(
@@ -111,7 +115,8 @@ class FHIRValueSetProviderHelper {
 					CanonicalUri.fromString(getOrNull(systemVersion)),
 					CanonicalUri.fromString(getOrNull(checkSystemVersion)),
 					CanonicalUri.fromString(getOrNull(forceSystemVersion)),
-					getOrNull(version));
+					getOrNull(version),
+					getOrNull(property));
 		} catch (URISyntaxException e) {
 			throw  FHIRHelper.exception("Invalid url parameter.", OperationOutcome.IssueType.INVALID, 400);
 		}
