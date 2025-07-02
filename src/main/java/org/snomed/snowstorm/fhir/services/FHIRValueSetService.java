b73f392340353dbe942a333d7bb21c8627e1e51d
pgwilliams <petergwilliams@hotmail.com>
2025-07-02 19:54:22 +0100

    ISTO-134 Code adjustment after removal of merge commits, to bring it back into line with original fhir-tests branch

--- src/main/java/org/snomed/snowstorm/fhir/services/FHIRValueSetService.java
+++ src/main/java/org/snomed/snowstorm/fhir/services/FHIRValueSetService.java
@@ -61,6 +61,7 @@ import static java.lang.Boolean.TRUE;
 import static java.lang.String.format;
 import static java.util.Collections.emptyList;
 import static org.apache.commons.collections4.MapUtils.emptyIfNull;
+import static org.snomed.snowstorm.config.Config.DEFAULT_LANGUAGE_CODE;
 import static org.ihtsdo.otf.RF2Constants.LANG_EN;
 import static org.snomed.snowstorm.core.data.services.ReferenceSetMemberService.AGGREGATION_MEMBER_COUNTS_BY_REFERENCE_SET;
 import static org.snomed.snowstorm.core.util.CollectionUtils.orEmpty;
