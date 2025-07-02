IHTSDO/snowstorm

Fetch latest snomed version when version not specified + improve doc and syndication import status handling.

src/main/java/org/snomed/snowstorm/syndication/SyndicationService.java
@@ -2,9 +2,9 @@ package org.snomed.snowstorm.syndication;
 
 import org.slf4j.Logger;
 import org.snomed.snowstorm.core.data.services.ServiceException;
-import org.snomed.snowstorm.syndication.common.SyndicationImportStatusService;
+import org.snomed.snowstorm.syndication.importstatus.SyndicationImportService;
 import org.snomed.snowstorm.syndication.common.SyndicationImportParams;
-import org.snomed.snowstorm.syndication.data.SyndicationImportStatus;
+import org.snomed.snowstorm.syndication.data.SyndicationImport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
@@ -14,6 +14,9 @@ import java.io.IOException;
 import java.util.List;
 
 import static org.apache.logging.log4j.util.Strings.isNotBlank;
+import static org.snomed.snowstorm.core.rf2.rf2import.ImportJob.ImportStatus.COMPLETED;
+import static org.snomed.snowstorm.core.rf2.rf2import.ImportJob.ImportStatus.FAILED;
+import static org.snomed.snowstorm.core.rf2.rf2import.ImportJob.ImportStatus.RUNNING;
 import static org.snomed.snowstorm.core.util.FileUtils.removeDownloadedFiles;
 import static org.snomed.snowstorm.syndication.common.SyndicationConstants.LATEST_VERSION;
 import static org.snomed.snowstorm.syndication.common.SyndicationConstants.LOCAL_VERSION;
@@ -22,7 +25,7 @@ import static org.snomed.snowstorm.syndication.common.SyndicationConstants.LOCAL
 public abstract class SyndicationService {
 
     @Autowired
-    protected SyndicationImportStatusService importStatusService;
+    protected SyndicationImportService importStatusService;
 
     protected final String terminologyName;
     protected final Logger logger;
@@ -33,53 +36,55 @@ public abstract class SyndicationService {
     }
 
     public void importTerminologyAndStoreResult(SyndicationImportParams params) throws IOException, ServiceException, InterruptedException {
-        SyndicationImportStatus status = importStatusService.getImportStatus(terminologyName);
+        SyndicationImport status = importStatusService.getImportStatus(terminologyName);
         if (alreadyImported(params, status)) {
             logger.info("Terminology {} version {} is already imported", terminologyName, status.getActualVersion());
             return;
         }
-        String importedTerminologyId = null;
+        String importedVersion = null;
         try {
+            importStatusService.saveOrUpdateImportStatus(terminologyName, params.getVersion(), importedVersion, RUNNING, null);
             List<File> files = fetchTerminologyPackages(params);
             if(CollectionUtils.isEmpty(files)) {
                 throw new ServiceException("No terminology packages found for imported version" + status.getActualVersion());
             }
             importTerminology(params, files);
-            importedTerminologyId = LOCAL_VERSION.equals(params.getVersion())
+            importedVersion = LOCAL_VERSION.equals(params.getVersion())
                     ? getLocalPackageIdentifier(files)
                     : getTerminologyVersion(files.get(files.size() - 1).getName());
-            importStatusService.saveOrUpdateImportStatus(terminologyName, params.getVersion(), importedTerminologyId, true, null);
+            importStatusService.saveOrUpdateImportStatus(terminologyName, params.getVersion(), importedVersion, COMPLETED, null);
             if(!LOCAL_VERSION.equals(params.getVersion())) {
                 removeDownloadedFiles(files);
             }
         } catch (Exception e) {
-            importStatusService.saveOrUpdateImportStatus(terminologyName, params.getVersion(), importedTerminologyId, false, e.getMessage());
+            importStatusService.saveOrUpdateImportStatus(terminologyName, params.getVersion(), importedVersion, FAILED, e.getMessage());
             throw e;
         }
     }
 
 
-    private boolean alreadyImported(SyndicationImportParams params, SyndicationImportStatus status) throws ServiceException, IOException, InterruptedException {
-        if (status != null && status.isSuccess() && isNotBlank(status.getActualVersion())) {
-            if (status.getActualVersion().equals(params.getVersion())) {
+    private boolean alreadyImported(SyndicationImportParams params, SyndicationImport syndicationImport) throws ServiceException, IOException, InterruptedException {
+        if (syndicationImport != null && syndicationImport.getStatus() == COMPLETED && isNotBlank(syndicationImport.getActualVersion())) {
+            if (syndicationImport.getActualVersion().equals(params.getVersion())) {
                 return true;
             }
             if(LOCAL_VERSION.equals(params.getVersion())) {
                 String localPackageIdentifier = getLocalPackageIdentifier(fetchTerminologyPackages(params));
-                return status.getActualVersion().equals(localPackageIdentifier);
+                return syndicationImport.getActualVersion().equals(localPackageIdentifier);
             }
-            if(LATEST_VERSION.equals(params.getVersion())) {
+            if(LATEST_VERSION.equals(params.getVersion()) || syndicationImport.getActualVersion().contains(params.getVersion())) {
                 logger.info("Fetching latest version number for terminology: {}", terminologyName);
                 try {
-                    String latestVersion = getLatestTerminologyVersion();
+                    String latestVersion = getLatestTerminologyVersion(params.getVersion());
                     logger.info("Latest terminology version: {}", latestVersion);
-                    return status.getActualVersion().equals(latestVersion);
+                    return syndicationImport.getActualVersion().equals(latestVersion);
                 }
                 catch (Exception e) {
                     logger.error("Failed to fetch latest version number for terminology: {}", terminologyName, e);
                 }
             }
         }
+        logger.info("Terminology: {} has not yet been imported", terminologyName);
         return false;
     }
 
@@ -95,7 +100,7 @@ public abstract class SyndicationService {
 
     protected abstract void importTerminology(SyndicationImportParams params, List<File> files) throws IOException, ServiceException, InterruptedException;
 
-    protected abstract String getLatestTerminologyVersion() throws IOException, InterruptedException;
+    protected abstract String getLatestTerminologyVersion(String params) throws IOException, InterruptedException, ServiceException;
 
     protected abstract String getTerminologyVersion(String releaseFileName) throws IOException;
 }

src/test/java/org/snomed/snowstorm/syndication/snomed/SnomedSyndicationServiceTest.java
@@ -12,22 +12,27 @@ import org.snomed.snowstorm.core.data.domain.CodeSystem;
 import org.snomed.snowstorm.core.data.services.CodeSystemService;
 import org.snomed.snowstorm.core.data.services.ServiceException;
 import org.snomed.snowstorm.core.rf2.rf2import.ImportService;
+import org.snomed.snowstorm.core.util.FileUtils;
 import org.snomed.snowstorm.syndication.common.SyndicationImportParams;
-import org.snomed.snowstorm.syndication.common.SyndicationImportStatusService;
+import org.snomed.snowstorm.syndication.importstatus.SyndicationImportService;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+import static org.snomed.snowstorm.syndication.common.SyndicationConstants.LOCAL_VERSION;
 
 @ExtendWith(MockitoExtension.class)
 class SnomedSyndicationServiceTest {
 
-    private static final String RELEASE_URI = "http://snomed.info/sct/11000172109/version/20250315";
+    private static final String RELEASE_VERSION_URI = "http://snomed.info/sct/11000172109/version/20250315";
+    private static final String RELEASE_URI = "http://snomed.info/sct/11000172109";
 
     @Mock
     private SnomedSyndicationClient syndicationClient;
@@ -39,7 +44,7 @@ class SnomedSyndicationServiceTest {
     private CodeSystemService codeSystemService;
 
     @Mock
-    private SyndicationImportStatusService importStatusService;
+    private SyndicationImportService importStatusService;
 
     @Spy
     @InjectMocks
@@ -58,21 +63,37 @@ class SnomedSyndicationServiceTest {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
                 syndicationService.importTerminologyAndStoreResult(new SyndicationImportParams(invalidUri, "BE", false)));
 
-        assertTrue(exception.getMessage().contains("not a valid SNOMED CT Edition Version URI"));
+        assertTrue(exception.getMessage().contains("not a valid SNOMED CT release URI"));
     }
 
     @Test
     void testImportSnomedEditionAndExtension_ValidURI_CallsImportMethods() throws IOException, ServiceException, ReleaseImportException, InterruptedException {
         List<File> filePaths = List.of(new File("edition.zip"), new File("extension.zip"));
 
-        when(syndicationClient.downloadPackages(RELEASE_URI, "testUser", "testPass")).thenReturn(filePaths);
+        when(syndicationClient.downloadPackages(RELEASE_VERSION_URI, "testUser", "testPass")).thenReturn(filePaths);
         doNothing().when(importService).importArchive(any(), any());
         doReturn(null).when(syndicationService).getFileInputStream(any());
 
-        syndicationService.importTerminologyAndStoreResult(new SyndicationImportParams(RELEASE_URI, "BE", false));
+        syndicationService.importTerminologyAndStoreResult(new SyndicationImportParams(RELEASE_VERSION_URI, "BE", false));
 
-        verify(syndicationClient).downloadPackages(RELEASE_URI, "testUser", "testPass");
+        verify(syndicationClient).downloadPackages(RELEASE_VERSION_URI, "testUser", "testPass");
         verify(importService, times(2)).importArchive(any(), any());
+    }
+
+    @Test
+    void testImportSnomedEditionAndExtension_Local_CallsImportMethods() throws IOException, ServiceException, ReleaseImportException, InterruptedException {
+        List<File> filePaths = List.of(new File("edition.zip"), new File("extension.zip"));
+
+        try(var fileUtilsMock = mockStatic(FileUtils.class)) {
+            fileUtilsMock.when(() -> FileUtils.findFile(any(), any()))
+                    .thenReturn(Optional.of(new File("edition.zip"))).thenReturn(Optional.of(new File("extension.zip")));
+            doNothing().when(importService).importArchive(any(), any());
+            doReturn(null).when(syndicationService).getFileInputStream(any());
+            syndicationService.importTerminologyAndStoreResult(new SyndicationImportParams(LOCAL_VERSION, "BE", false));
+
+            verify(syndicationClient, never()).downloadPackages(any(), any(), any());
+            verify(importService, times(2)).importArchive(any(), any());
+        }
     }
 
     @Test
@@ -81,7 +102,7 @@ class SnomedSyndicationServiceTest {
 
 
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
-                syndicationService.importTerminologyAndStoreResult(new SyndicationImportParams(RELEASE_URI, "BE", false)));
+                syndicationService.importTerminologyAndStoreResult(new SyndicationImportParams(RELEASE_VERSION_URI, "BE", false)));
 
         assertEquals("Syndication username is blank.", exception.getMessage());
     }
@@ -92,7 +113,7 @@ class SnomedSyndicationServiceTest {
 
 
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
-                syndicationService.importTerminologyAndStoreResult(new SyndicationImportParams(RELEASE_URI, "BE", false)));
+                syndicationService.importTerminologyAndStoreResult(new SyndicationImportParams(RELEASE_VERSION_URI, "BE", false)));
 
         assertEquals("Syndication password is blank.", exception.getMessage());
     }
@@ -102,7 +123,7 @@ class SnomedSyndicationServiceTest {
         List<String> filePaths = List.of("edition.zip", "extension.zip");
 
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
-                ReflectionTestUtils.invokeMethod(syndicationService, "importExtension", RELEASE_URI, null, filePaths));
+                ReflectionTestUtils.invokeMethod(syndicationService, "importExtension", RELEASE_VERSION_URI, null, filePaths));
 
         assertTrue(exception.getMessage().contains("An extension name must be specified."));
     }
@@ -112,8 +133,28 @@ class SnomedSyndicationServiceTest {
         List<String> filePaths = List.of("edition.zip", "extension.zip");
         doReturn(null).when(syndicationService).getFileInputStream(any());
 
-        ReflectionTestUtils.invokeMethod(syndicationService, "importExtension", RELEASE_URI, "BE", filePaths);
+        ReflectionTestUtils.invokeMethod(syndicationService, "importExtension", RELEASE_VERSION_URI, "BE", filePaths);
 
         verify(codeSystemService).createCodeSystem(any(CodeSystem.class));
+    }
+
+    @Test
+    void testGetLatestTerminologyVersion_success() throws IOException {
+        SyndicationFeed syndicationFeed = new SyndicationFeed();
+        SyndicationFeedEntry syndicationFeedEntry = new SyndicationFeedEntry();
+        syndicationFeedEntry.setContentItemVersion(RELEASE_VERSION_URI);
+        syndicationFeed.setEntries(List.of(syndicationFeedEntry));
+        doReturn(syndicationFeed).when(syndicationClient).getFeed();
+
+        String latestVersion = ReflectionTestUtils.invokeMethod(syndicationService, "getLatestTerminologyVersion", RELEASE_URI);
+
+        assertEquals(RELEASE_VERSION_URI, latestVersion);
+    }
+
+    @Test
+    void testGetLatestTerminologyVersion_failure() throws IOException {
+        doReturn(new SyndicationFeed()).when(syndicationClient).getFeed();
+
+        assertThrows(Exception.class, () -> ReflectionTestUtils.invokeMethod(syndicationService, "getLatestTerminologyVersion", RELEASE_URI));
     }
 }

IHTSDO/snowstorm-x

src/main/java/org/snomed/snowstorm/syndication/SyndicationService.java
@@ -2,9 +2,9 @@ package org.snomed.snowstorm.syndication;
 
 import org.slf4j.Logger;
 import org.snomed.snowstorm.core.data.services.ServiceException;
-import org.snomed.snowstorm.syndication.common.SyndicationImportStatusService;
+import org.snomed.snowstorm.syndication.importstatus.SyndicationImportService;
 import org.snomed.snowstorm.syndication.common.SyndicationImportParams;
-import org.snomed.snowstorm.syndication.data.SyndicationImportStatus;
+import org.snomed.snowstorm.syndication.data.SyndicationImport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
@@ -14,6 +14,9 @@ import java.io.IOException;
 import java.util.List;
 
 import static org.apache.logging.log4j.util.Strings.isNotBlank;
+import static org.snomed.snowstorm.core.rf2.rf2import.ImportJob.ImportStatus.COMPLETED;
+import static org.snomed.snowstorm.core.rf2.rf2import.ImportJob.ImportStatus.FAILED;
+import static org.snomed.snowstorm.core.rf2.rf2import.ImportJob.ImportStatus.RUNNING;
 import static org.snomed.snowstorm.core.util.FileUtils.removeDownloadedFiles;
 import static org.snomed.snowstorm.syndication.common.SyndicationConstants.LATEST_VERSION;
 import static org.snomed.snowstorm.syndication.common.SyndicationConstants.LOCAL_VERSION;
@@ -22,7 +25,7 @@ import static org.snomed.snowstorm.syndication.common.SyndicationConstants.LOCAL
 public abstract class SyndicationService {
 
     @Autowired
-    protected SyndicationImportStatusService importStatusService;
+    protected SyndicationImportService importStatusService;
 
     protected final String terminologyName;
     protected final Logger logger;
@@ -32,54 +35,56 @@ public abstract class SyndicationService {
         this.logger = logger;
     }
 
-    public void importTerminologyAndStoreResult(SyndicationImportParams params) throws IOException, ServiceException, InterruptedException {
-        SyndicationImportStatus status = importStatusService.getImportStatus(terminologyName);
+    public void fetchAndImportTerminology(SyndicationImportParams params) throws IOException, ServiceException, InterruptedException {
+        SyndicationImport status = importStatusService.getImportStatus(terminologyName);
         if (alreadyImported(params, status)) {
             logger.info("Terminology {} version {} is already imported", terminologyName, status.getActualVersion());
             return;
         }
-        String importedTerminologyId = null;
+        String importedVersion = null;
         try {
+            importStatusService.saveOrUpdateImportStatus(terminologyName, params.version(), importedVersion, RUNNING, null);
             List<File> files = fetchTerminologyPackages(params);
             if(CollectionUtils.isEmpty(files)) {
                 throw new ServiceException("No terminology packages found for imported version" + status.getActualVersion());
             }
             importTerminology(params, files);
-            importedTerminologyId = LOCAL_VERSION.equals(params.getVersion())
+            importedVersion = LOCAL_VERSION.equals(params.version())
                     ? getLocalPackageIdentifier(files)
                     : getTerminologyVersion(files.get(files.size() - 1).getName());
-            importStatusService.saveOrUpdateImportStatus(terminologyName, params.getVersion(), importedTerminologyId, true, null);
-            if(!LOCAL_VERSION.equals(params.getVersion())) {
+            importStatusService.saveOrUpdateImportStatus(terminologyName, params.version(), importedVersion, COMPLETED, null);
+            if(!LOCAL_VERSION.equals(params.version())) {
                 removeDownloadedFiles(files);
             }
         } catch (Exception e) {
-            importStatusService.saveOrUpdateImportStatus(terminologyName, params.getVersion(), importedTerminologyId, false, e.getMessage());
+            importStatusService.saveOrUpdateImportStatus(terminologyName, params.version(), importedVersion, FAILED, e.getMessage());
             throw e;
         }
     }
 
 
-    private boolean alreadyImported(SyndicationImportParams params, SyndicationImportStatus status) throws ServiceException, IOException, InterruptedException {
-        if (status != null && status.isSuccess() && isNotBlank(status.getActualVersion())) {
-            if (status.getActualVersion().equals(params.getVersion())) {
+    private boolean alreadyImported(SyndicationImportParams params, SyndicationImport syndicationImport) throws ServiceException, IOException, InterruptedException {
+        if (syndicationImport != null && syndicationImport.getStatus() == COMPLETED && isNotBlank(syndicationImport.getActualVersion())) {
+            if (syndicationImport.getActualVersion().equals(params.version())) {
                 return true;
             }
-            if(LOCAL_VERSION.equals(params.getVersion())) {
+            if(LOCAL_VERSION.equals(params.version())) {
                 String localPackageIdentifier = getLocalPackageIdentifier(fetchTerminologyPackages(params));
-                return status.getActualVersion().equals(localPackageIdentifier);
+                return syndicationImport.getActualVersion().equals(localPackageIdentifier);
             }
-            if(LATEST_VERSION.equals(params.getVersion())) {
+            if(LATEST_VERSION.equals(params.version()) || syndicationImport.getActualVersion().contains(params.version())) {
                 logger.info("Fetching latest version number for terminology: {}", terminologyName);
                 try {
-                    String latestVersion = getLatestTerminologyVersion();
+                    String latestVersion = getLatestTerminologyVersion(params.version());
                     logger.info("Latest terminology version: {}", latestVersion);
-                    return status.getActualVersion().equals(latestVersion);
+                    return syndicationImport.getActualVersion().equals(latestVersion);
                 }
                 catch (Exception e) {
                     logger.error("Failed to fetch latest version number for terminology: {}", terminologyName, e);
                 }
             }
         }
+        logger.info("Terminology: {} has not yet been imported", terminologyName);
         return false;
     }
 
@@ -95,7 +100,7 @@ public abstract class SyndicationService {
 
     protected abstract void importTerminology(SyndicationImportParams params, List<File> files) throws IOException, ServiceException, InterruptedException;
 
-    protected abstract String getLatestTerminologyVersion() throws IOException, InterruptedException;
+    protected abstract String getLatestTerminologyVersion(String params) throws IOException, InterruptedException, ServiceException;
 
     protected abstract String getTerminologyVersion(String releaseFileName) throws IOException;
 }

src/test/java/org/snomed/snowstorm/syndication/snomed/SnomedSyndicationServiceTest.java
@@ -12,22 +12,27 @@ import org.snomed.snowstorm.core.data.domain.CodeSystem;
 import org.snomed.snowstorm.core.data.services.CodeSystemService;
 import org.snomed.snowstorm.core.data.services.ServiceException;
 import org.snomed.snowstorm.core.rf2.rf2import.ImportService;
+import org.snomed.snowstorm.core.util.FileUtils;
 import org.snomed.snowstorm.syndication.common.SyndicationImportParams;
-import org.snomed.snowstorm.syndication.common.SyndicationImportStatusService;
+import org.snomed.snowstorm.syndication.importstatus.SyndicationImportService;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+import static org.snomed.snowstorm.syndication.common.SyndicationConstants.LOCAL_VERSION;
 
 @ExtendWith(MockitoExtension.class)
 class SnomedSyndicationServiceTest {
 
-    private static final String RELEASE_URI = "http://snomed.info/sct/11000172109/version/20250315";
+    private static final String RELEASE_VERSION_URI = "http://snomed.info/sct/11000172109/version/20250315";
+    private static final String RELEASE_URI = "http://snomed.info/sct/11000172109";
 
     @Mock
     private SnomedSyndicationClient syndicationClient;
@@ -39,7 +44,7 @@ class SnomedSyndicationServiceTest {
     private CodeSystemService codeSystemService;
 
     @Mock
-    private SyndicationImportStatusService importStatusService;
+    private SyndicationImportService importStatusService;
 
     @Spy
     @InjectMocks
@@ -56,23 +61,39 @@ class SnomedSyndicationServiceTest {
         String invalidUri = "invalid-uri";
 
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
-                syndicationService.importTerminologyAndStoreResult(new SyndicationImportParams(invalidUri, "BE", false)));
+                syndicationService.fetchAndImportTerminology(new SyndicationImportParams(invalidUri, "BE", false)));
 
-        assertTrue(exception.getMessage().contains("not a valid SNOMED CT Edition Version URI"));
+        assertTrue(exception.getMessage().contains("not a valid SNOMED CT release URI"));
     }
 
     @Test
     void testImportSnomedEditionAndExtension_ValidURI_CallsImportMethods() throws IOException, ServiceException, ReleaseImportException, InterruptedException {
         List<File> filePaths = List.of(new File("edition.zip"), new File("extension.zip"));
 
-        when(syndicationClient.downloadPackages(RELEASE_URI, "testUser", "testPass")).thenReturn(filePaths);
+        when(syndicationClient.downloadPackages(RELEASE_VERSION_URI, "testUser", "testPass")).thenReturn(filePaths);
         doNothing().when(importService).importArchive(any(), any());
         doReturn(null).when(syndicationService).getFileInputStream(any());
 
-        syndicationService.importTerminologyAndStoreResult(new SyndicationImportParams(RELEASE_URI, "BE", false));
+        syndicationService.fetchAndImportTerminology(new SyndicationImportParams(RELEASE_VERSION_URI, "BE", false));
 
-        verify(syndicationClient).downloadPackages(RELEASE_URI, "testUser", "testPass");
+        verify(syndicationClient).downloadPackages(RELEASE_VERSION_URI, "testUser", "testPass");
         verify(importService, times(2)).importArchive(any(), any());
+    }
+
+    @Test
+    void testImportSnomedEditionAndExtension_Local_CallsImportMethods() throws IOException, ServiceException, ReleaseImportException, InterruptedException {
+        List<File> filePaths = List.of(new File("edition.zip"), new File("extension.zip"));
+
+        try(var fileUtilsMock = mockStatic(FileUtils.class)) {
+            fileUtilsMock.when(() -> FileUtils.findFile(any(), any()))
+                    .thenReturn(Optional.of(new File("edition.zip"))).thenReturn(Optional.of(new File("extension.zip")));
+            doNothing().when(importService).importArchive(any(), any());
+            doReturn(null).when(syndicationService).getFileInputStream(any());
+            syndicationService.fetchAndImportTerminology(new SyndicationImportParams(LOCAL_VERSION, "BE", false));
+
+            verify(syndicationClient, never()).downloadPackages(any(), any(), any());
+            verify(importService, times(2)).importArchive(any(), any());
+        }
     }
 
     @Test
@@ -81,7 +102,7 @@ class SnomedSyndicationServiceTest {
 
 
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
-                syndicationService.importTerminologyAndStoreResult(new SyndicationImportParams(RELEASE_URI, "BE", false)));
+                syndicationService.fetchAndImportTerminology(new SyndicationImportParams(RELEASE_VERSION_URI, "BE", false)));
 
         assertEquals("Syndication username is blank.", exception.getMessage());
     }
@@ -92,7 +113,7 @@ class SnomedSyndicationServiceTest {
 
 
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
-                syndicationService.importTerminologyAndStoreResult(new SyndicationImportParams(RELEASE_URI, "BE", false)));
+                syndicationService.fetchAndImportTerminology(new SyndicationImportParams(RELEASE_VERSION_URI, "BE", false)));
 
         assertEquals("Syndication password is blank.", exception.getMessage());
     }
@@ -102,7 +123,7 @@ class SnomedSyndicationServiceTest {
         List<String> filePaths = List.of("edition.zip", "extension.zip");
 
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
-                ReflectionTestUtils.invokeMethod(syndicationService, "importExtension", RELEASE_URI, null, filePaths));
+                ReflectionTestUtils.invokeMethod(syndicationService, "importExtension", RELEASE_VERSION_URI, null, filePaths));
 
         assertTrue(exception.getMessage().contains("An extension name must be specified."));
     }
@@ -112,8 +133,28 @@ class SnomedSyndicationServiceTest {
         List<String> filePaths = List.of("edition.zip", "extension.zip");
         doReturn(null).when(syndicationService).getFileInputStream(any());
 
-        ReflectionTestUtils.invokeMethod(syndicationService, "importExtension", RELEASE_URI, "BE", filePaths);
+        ReflectionTestUtils.invokeMethod(syndicationService, "importExtension", RELEASE_VERSION_URI, "BE", filePaths);
 
         verify(codeSystemService).createCodeSystem(any(CodeSystem.class));
+    }
+
+    @Test
+    void testGetLatestTerminologyVersion_success() throws IOException {
+        SyndicationFeed syndicationFeed = new SyndicationFeed();
+        SyndicationFeedEntry syndicationFeedEntry = new SyndicationFeedEntry();
+        syndicationFeedEntry.setContentItemVersion(RELEASE_VERSION_URI);
+        syndicationFeed.setEntries(List.of(syndicationFeedEntry));
+        doReturn(syndicationFeed).when(syndicationClient).getFeed();
+
+        String latestVersion = ReflectionTestUtils.invokeMethod(syndicationService, "getLatestTerminologyVersion", RELEASE_URI);
+
+        assertEquals(RELEASE_VERSION_URI, latestVersion);
+    }
+
+    @Test
+    void testGetLatestTerminologyVersion_failure() throws IOException {
+        doReturn(new SyndicationFeed()).when(syndicationClient).getFeed();
+
+        assertThrows(Exception.class, () -> ReflectionTestUtils.invokeMethod(syndicationService, "getLatestTerminologyVersion", RELEASE_URI));
     }
 }